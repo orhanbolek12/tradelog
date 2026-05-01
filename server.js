5fc6d0871621d3a6a6c330a6c060618db754dbc2
Orhan Bölek <orhan.bolek@ace.local>
2026-05-01 16:30:23 +0300

    Fix Express 5 routing crash on catch-all route

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/trades', tradeRoutes);
 app.use('/api/lookup', lookupRoutes);
 
 // SPA fallback
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
